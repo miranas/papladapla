cc6ad6378e5231948a9139a6d327bcb76dd8b513
miranas <miran.asanin@gmail.com>
2019-03-14 13:50:08 +0100

    začetni komit

---
+++ MVCFilmi/Controllers/FilmController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCFilmi.Models;
+
+namespace MVCFilmi.Controllers
+{
+    public class FilmController : Controller
+    {
+        private MVCFilmContext db = new MVCFilmContext();
+
+        // GET: Film
+        public ActionResult Index()
+        {
+            return View(db.Films.ToList());
+        }
+
+        // GET: Film/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Film film = db.Films.Find(id);
+            if (film == null)
+            {
+                return HttpNotFound();
+            }
+            return View(film);
+        }
+
+        // GET: Film/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Film/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,naslov,Izdano,Tip,Cena")] Film film)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Films.Add(film);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(film);
+        }
+
+        // GET: Film/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Film film = db.Films.Find(id);
+            if (film == null)
+            {
+                return HttpNotFound();
+            }
+            return View(film);
+        }
+
+        // POST: Film/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,naslov,Izdano,Tip,Cena")] Film film)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(film).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(film);
+        }
+
+        // GET: Film/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Film film = db.Films.Find(id);
+            if (film == null)
+            {
+                return HttpNotFound();
+            }
+            return View(film);
+        }
+
+        // POST: Film/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Film film = db.Films.Find(id);
+            db.Films.Remove(film);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Isci(string niz)
+        {
+            var filmi = from a in db.Films
+                        select a;
+            if (!String.IsNullOrEmpty(niz))
+            {
+                filmi = from a in filmi
+                        where a.naslov.Contains(niz)
+                        select a;
+            }
+            return View(filmi);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
